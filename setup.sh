5dd491c66d46f7e992ab9ddb649a26fb9d6c8209
CBHue <CBHue@users.noreply.github.com>
2017-04-14 18:18:47 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 ##### (Cosmetic) Colour output
-RED     = "\033[01;31m"   # Issues/Errors
-GREEN   = "\033[01;32m"   # Success
-YELLOW  = "\033[01;33m"   # Warnings/Information
-BLUE    = "\033[01;34m"   # Heading
-BOLD    = "\033[01;01m"   # Highlight
-RESET   = "\033[00m"      # Normal
+RED="\033[01;31m"      # Issues/Errors
+GREEN="\033[01;32m"    # Success
+YELLOW="\033[01;33m"   # Warnings/Information
+BLUE="\033[01;34m"     # Heading
+BOLD="\033[01;01m"     # Highlight
+RESET="\033[00m"       # Normal
 
 #update everything 
 apt-get -qq update && apt-get -y -qq dist-upgrade --fix-missing
