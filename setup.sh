a86685ea895d1f66e2cbdc61fdc597970a783b0b
CBHue <CBHue@users.noreply.github.com>
2018-07-18 11:15:40 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -200,7 +200,7 @@ alias src="source ~/.bashrc"
 alias msfc="service postgresql start; msfconsole"
 
 # Were going to override the promt ... Should be done in profile ... but oh well
-PS1="\[\033[31m\][\[\033[36m\]\u\[\033[31m\]]\[\033[31m\]\h:\[\033[33;1m\]\W\[\033[m\] 🤬 : "
+PS1="\[\033[31m\][\[\033[36m\]\u\[\033[31m\]]\[\033[31m\]\h:\[\033[33;1m\]\w\[\033[m\] 🤬 : "
 
 EOF
 
