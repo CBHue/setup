c2c79eaf5cf05ec53283a44d838894b865c5cf2c
CBHue <CBHue@users.noreply.github.com>
2018-08-02 23:26:08 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -95,6 +95,7 @@ easyGIT=(
 	https://github.com/byt3bl33d3r/DeathStar
 	https://github.com/CBHue/prepList.git
 	https://github.com/CBHue/ipListER.git
+	https://github.com/CBHue/Recon-da.git
 	https://github.com/Coalfire-Research/Doozer.git
 	https://github.com/Coalfire-Research/java-deserialization-exploits.git
 	https://github.com/CoreSecurity/impacket.git
