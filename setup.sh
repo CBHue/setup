f945670f482d7e3fcb1663c478a5cd7d69020762
CBHue <CBHue@users.noreply.github.com>
2018-08-10 13:26:46 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -28,6 +28,8 @@ for x in $( tr '[:upper:]' '[:lower:]' <<< "$@" ); do
     distupgrade=true
   elif [ "${x}" == "--upgrade" ]; then
     upgrade=true
+  elif [ "${x}" == "--visual" ]; then
+    visual=true
   else
     echo -e ' '$RED'[!]'$RESET' Unknown option: '${x} 1>&2
     exit 1
@@ -49,6 +51,13 @@ if [ "$dist" == "true" ]; then
   apt-get -y -qq dist-upgrade --fix-missing
 else
   echo -e ' '$RED'[!]'$RESET' Skipping apt-get dist-upgrade ... [--distupgrade]' 1>&2
+fi
+
+# install background?
+if [ "$visual" == "true" ]; then
+  
+else
+  echo -e ' '$RED'[!]'$RESET' Skipping visuals ... [--visual]' 1>&2
 fi
 
 # Sources
@@ -184,234 +193,236 @@ fi
 echo -e "\n $GREEN[+]$RESET Installing Webdav Server"
 pip install cheroot wsgidav
 
-# Wallpaper ... Assuming you pulled the paper as well
-wPaperDIR="$(dirname "$(readlink -f "$0")")/"
-wPaper="$(ls $wPaperDIR |sort -R | egrep "png|jpg" |tail -1)"
+function prettyInstall {
+	# Wallpaper ... Assuming you pulled the paper as well
+	wPaperDIR="$(dirname "$(readlink -f "$0")")/"
+	wPaper="$(ls $wPaperDIR |sort -R | egrep "png|jpg" |tail -1)"
 
-if [ -f $wPaper ]; then
-  gsettings set org.gnome.desktop.background picture-uri file://${wPaperDIR}/${wPaper}
-  gsettings set org.gnome.desktop.background picture-options "stretched"
-  
-  # Need to check which desktop you are using ...
-  #xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s ${wPaperDIR}/${wPaper}
-fi
+	if [ -f $wPaper ]; then
+	  gsettings set org.gnome.desktop.background picture-uri file://${wPaperDIR}/${wPaper}
+	  gsettings set org.gnome.desktop.background picture-options "stretched"
 
-# Lets write some config files #
-file=/root/.bash_aliases;
-cat <<EOF > "$file"
-alias chrome="chromium --no-sandbox --user-data-dir /tmp --password-store=basic 2>/dev/null &"
-alias chromeProxy="chromium --no-sandbox --user-data-dir /tmp --password-store=basic --proxy-server=127.0.0.1:8080 2>/dev/null &"
-alias httpServer="ifconfig; python -m SimpleHTTPServer"
-alias ftpServer="ifconfig; python -m pyftpdlib -p 21 -w"
-alias soapui="/opt/SoapUI-5.2.1/bin/soapui.sh 2>/dev/null &"
-alias pubIP="dig +short myip.opendns.com @resolver1.opendns.com"
-alias src="source ~/.bashrc"
-alias msfc="service postgresql start; msfconsole"
+	  # Need to check which desktop you are using ...
+	  #xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s ${wPaperDIR}/${wPaper}
+	fi
 
-# Were going to override the promt ... Should be done in profile ... but oh well
-PS1="\[\033[31m\][\[\033[36m\]\u\[\033[31m\]]\[\033[31m\]\h:\[\033[33;1m\]\w\[\033[m\] 🤬 : "
+	# Lets write some config files #
+	file=/root/.bash_aliases;
+	cat <<EOF > "$file"
+	alias chrome="chromium --no-sandbox --user-data-dir /tmp --password-store=basic 2>/dev/null &"
+	alias chromeProxy="chromium --no-sandbox --user-data-dir /tmp --password-store=basic --proxy-server=127.0.0.1:8080 2>/dev/null &"
+	alias httpServer="ifconfig; python -m SimpleHTTPServer"
+	alias ftpServer="ifconfig; python -m pyftpdlib -p 21 -w"
+	alias soapui="/opt/SoapUI-5.2.1/bin/soapui.sh 2>/dev/null &"
+	alias pubIP="dig +short myip.opendns.com @resolver1.opendns.com"
+	alias src="source ~/.bashrc"
+	alias msfc="service postgresql start; msfconsole"
 
-EOF
+	# Were going to override the promt ... Should be done in profile ... but oh well
+	PS1="\[\033[31m\][\[\033[36m\]\u\[\033[31m\]]\[\033[31m\]\h:\[\033[33;1m\]\w\[\033[m\] 🤬 : "
 
-file=/root/.tmux.conf;
-cat <<EOF > "$file"
-# TMUX 4 the WIN!!
-unbind %
-bind | split-window -h
-bind - split-window -v
+	EOF
 
-# Set status bar
-set -g status-bg black
-set -g status-fg white
-set -g status-left-length 90
-set -g status-right-length 60
-set -g window-status-current-attr bold
-set -g status-interval 60
-set -g status-justify left
-set -g status-left '#[fg=white]</ #(ifconfig eth0 | grep netmask | cut -d" " -f10) > '
-set -g status-right '#[fg=blue]#S #[fg=white]%a %d %b %R ' 
+	file=/root/.tmux.conf;
+	cat <<EOF > "$file"
+	# TMUX 4 the WIN!!
+	unbind %
+	bind | split-window -h
+	bind - split-window -v
 
-# Highlight active window
-set-window-option -g window-status-current-bg black 
-set-window-option -g window-status-current-fg yellow 
+	# Set status bar
+	set -g status-bg black
+	set -g status-fg white
+	set -g status-left-length 90
+	set -g status-right-length 60
+	set -g window-status-current-attr bold
+	set -g status-interval 60
+	set -g status-justify left
+	set -g status-left '#[fg=white]</ #(ifconfig eth0 | grep netmask | cut -d" " -f10) > '
+	set -g status-right '#[fg=blue]#S #[fg=white]%a %d %b %R ' 
 
-# Set window notifications
-setw -g monitor-activity on
-set -g visual-activity on
+	# Highlight active window
+	set-window-option -g window-status-current-bg black 
+	set-window-option -g window-status-current-fg yellow 
 
-# Automatically set window title
-setw -g automatic-rename on
-set-option -g set-titles on
+	# Set window notifications
+	setw -g monitor-activity on
+	set -g visual-activity on
 
-# Histories
-set -g history-limit 10000
+	# Automatically set window title
+	setw -g automatic-rename on
+	set-option -g set-titles on
 
-# Copy mode
-setw -g mouse on
+	# Histories
+	set -g history-limit 10000
 
-# Use Alt-arrow keys without prefix key to switch panes
-bind -n M-Left select-pane -L
-bind -n M-Right select-pane -R
-bind -n M-Up select-pane -U
-bind -n M-Down select-pane -D
+	# Copy mode
+	setw -g mouse on
 
-# Shift arrow to switch windows
-bind -n S-Left  previous-window
-bind -n S-Right next-window
-EOF
+	# Use Alt-arrow keys without prefix key to switch panes
+	bind -n M-Left select-pane -L
+	bind -n M-Right select-pane -R
+	bind -n M-Up select-pane -U
+	bind -n M-Down select-pane -D
 
-#--- Configure conky
-file=/root/.conkyrc_right
-cat <<EOF > "/root/.conkyrc_right"
-# Conky Right
-background no
-use_xft yes
-xftalpha 0.6
-own_window true
-own_window_type desktop
-own_window_argb_visual true
-# 0 = transparent, 255 = solid
-own_window_argb_value 160 
-double_buffer yes
-update_interval 1
-#maximum_width 400
-alignment top_right
-#gap_x 50
-gap_y 110
-no_buffers yes
-uppercase no
-cpu_avg_samples 5
-net_avg_samples 5
-diskio_avg_samples 5
-if_up_strictness address
-draw_shades no
-draw_outline no
-draw_borders no
-draw_graph_borders yes
-default_color lightgray
-default_shade_color red
-default_outline_color green
-short_units true
-use_spacer none
-xftfont DejaVu Sans Mono:size=10
-TEXT
-\${color white}System \$hr
-\${color green}Hostname: \${color red}\${exec whoami} @ \$nodename
-\${color green}Kernel:   \$kernel
-\${color green}Uptime:\$color   \$uptime
-\${color white}Processor \$hr
-\${color green}CPU Freq:\$color  \$freq MHz
-\${color green}CPU Usage:\$color \$cpu% \${cpubar 4}
-\${color green}Name                      PID   CPU%   MEM%
-\${color green} 1: \${top name 1} \${top pid 1} \${top cpu 1} \${top mem 1}
-\${color green} 2: \${top name 2} \${top pid 2} \${top cpu 2} \${top mem 2}
-\${color green} 3: \${top name 3} \${top pid 3} \${top cpu 3} \${top mem 3}
-\${color green} 4: \${top name 4} \${top pid 4} \${top cpu 4} \${top mem 4}
-\${color green} 5: \${top name 5} \${top pid 5} \${top cpu 5} \${top mem 5}
-\${color green} 6: \${top name 6} \${top pid 6} \${top cpu 6} \${top mem 6}
-\${color green} 7: \${top name 7} \${top pid 7} \${top cpu 7} \${top mem 7}
-\${color green}Processes:\$color \$processes  \${color}Running:\$color \$running_processes
-\${color white}Memory \$hr
-\${color green}\${memgraph 23,350 000000 800000 -t}
-\${color green}RAM Usage:\$color  \$mem/\$memmax - \$memperc% 
-\${membar 4}
-\${color green}Swap Usage:\$color \$swap/\$swapmax - \$swapperc% 
-\${swapbar 4}
-\${color white}Storage \$hr
-\${color green}File systems: / \$color\${fs_used /}/\${fs_size /} \${fs_bar 6 /}
-EOF
+	# Shift arrow to switch windows
+	bind -n S-Left  previous-window
+	bind -n S-Right next-window
+	EOF
 
-cat <<EOF > "/root/.conkyrc_left"
-# Conky Left
-background no
-use_xft yes
-xftalpha 0.6
-own_window true
-own_window_type desktop
-own_window_argb_visual true
-# 0 = transparent, 255 = solid
-own_window_argb_value 160 
-double_buffer yes
-update_interval 1
-#maximum_width 400
-alignment top_left
-gap_x 60
-gap_y 110
-no_buffers yes
-uppercase no
-cpu_avg_samples 5
-net_avg_samples 5
-diskio_avg_samples 5
-if_up_strictness address
-draw_shades no
-draw_outline no
-draw_borders no
-draw_graph_borders yes
-default_color lightgray
-default_shade_color red
-default_outline_color green
-short_units true
-use_spacer none
-xftfont DejaVu Sans Mono:size=10
-TEXT
-\${color white}Networking \$hr
-# Start eth0
-\${if_up eth0}\${color green}eth0 \$color \${addr eth0}
-\${color green}eth0 \$color \${exec ifconfig eth0| grep ether | cut -d" " -f10 } \${endif}
-#
-# Start wlan0
-\${if_up wlan0}
-\${color green}wlan0 \$color \${exec iwgetid -r} 
-\${color green}wlan0 \$color \${addr wlan0}
-\${color green}wlan0 \$color \${exec ifconfig wlan0| grep ether | cut -d" " -f10 } 
-#
-\${color white}Upload Gateway: \$color\${upspeedf wlan0}Kb/s
-\${color green}\${upspeedgraph wlan0 20,350 0000ff ff0000 -t}
-\${color white}Download Gateway: \$color\${downspeedf wlan0}Kb/s
-\${color green}\${downspeedgraph wlan0 20,350 0000ff ff0000 -t}
-\${endif}
-#
-# Start ra0
-\${if_up ra0}
-\${color green}ra0 \$color \${exec iwgetid -r} 
-\${color green}ra0 \$color \${addr ra0}
-\${color green}ra0 \$color \${exec ifconfig ra0| grep ether | cut -d" " -f10 } 
-\${color white}Upload Gateway: \$color\${upspeedf ra0}Kb/s
-\${color green}\${upspeedgraph ra0 20,350 0000ff ff0000 -t}
-\${color white}Download Gateway: \$color\${downspeedf ra0}Kb/s
-\${color green}\${downspeedgraph ra0 20,350 0000ff ff0000 -t}
-\${endif}
-# Start tun0
-\${if_up tun0}\${color green}tun0 \$color \${addr tun0} \${endif}
-\${color white}Listening TCP:
-\${color green}\${execi 10 netstat -anlp | grep LISTEN | grep -v ING | awk -F" " '{printf "%-5s %-15s %-15s %-15s\n", \$1, \$4, \$5, \$7}'}
-\${color white}Listening UDP:
-\${color green}\${execi 10 netstat -anulp | egrep -v "udp6|Proto|\(servers|ESTABLISHED" | awk -F" " '{printf "%-5s %-15s %-15s %-15s\n", \$1, \$4, \$5, \$6}'}
-EOF
+	#--- Configure conky
+	file=/root/.conkyrc_right
+	cat <<EOF > "/root/.conkyrc_right"
+	# Conky Right
+	background no
+	use_xft yes
+	xftalpha 0.6
+	own_window true
+	own_window_type desktop
+	own_window_argb_visual true
+	# 0 = transparent, 255 = solid
+	own_window_argb_value 160 
+	double_buffer yes
+	update_interval 1
+	#maximum_width 400
+	alignment top_right
+	#gap_x 50
+	gap_y 110
+	no_buffers yes
+	uppercase no
+	cpu_avg_samples 5
+	net_avg_samples 5
+	diskio_avg_samples 5
+	if_up_strictness address
+	draw_shades no
+	draw_outline no
+	draw_borders no
+	draw_graph_borders yes
+	default_color lightgray
+	default_shade_color red
+	default_outline_color green
+	short_units true
+	use_spacer none
+	xftfont DejaVu Sans Mono:size=10
+	TEXT
+	\${color white}System \$hr
+	\${color green}Hostname: \${color red}\${exec whoami} @ \$nodename
+	\${color green}Kernel:   \$kernel
+	\${color green}Uptime:\$color   \$uptime
+	\${color white}Processor \$hr
+	\${color green}CPU Freq:\$color  \$freq MHz
+	\${color green}CPU Usage:\$color \$cpu% \${cpubar 4}
+	\${color green}Name                      PID   CPU%   MEM%
+	\${color green} 1: \${top name 1} \${top pid 1} \${top cpu 1} \${top mem 1}
+	\${color green} 2: \${top name 2} \${top pid 2} \${top cpu 2} \${top mem 2}
+	\${color green} 3: \${top name 3} \${top pid 3} \${top cpu 3} \${top mem 3}
+	\${color green} 4: \${top name 4} \${top pid 4} \${top cpu 4} \${top mem 4}
+	\${color green} 5: \${top name 5} \${top pid 5} \${top cpu 5} \${top mem 5}
+	\${color green} 6: \${top name 6} \${top pid 6} \${top cpu 6} \${top mem 6}
+	\${color green} 7: \${top name 7} \${top pid 7} \${top cpu 7} \${top mem 7}
+	\${color green}Processes:\$color \$processes  \${color}Running:\$color \$running_processes
+	\${color white}Memory \$hr
+	\${color green}\${memgraph 23,350 000000 800000 -t}
+	\${color green}RAM Usage:\$color  \$mem/\$memmax - \$memperc% 
+	\${membar 4}
+	\${color green}Swap Usage:\$color \$swap/\$swapmax - \$swapperc% 
+	\${swapbar 4}
+	\${color white}Storage \$hr
+	\${color green}File systems: / \$color\${fs_used /}/\${fs_size /} \${fs_bar 6 /}
+	EOF
 
-mkdir -p /root/.config/autostart/
-cat <<EOF > "/root/.config/autostart/conkyrc_right.desktop"
-[Desktop Entry]
-Type=Application
-Exec=/usr/bin/conky -q -c /root/.conkyrc_right
-Hidden=false
-NoDisplay=false
-X-GNOME-Autostart-enabled=true
-Name=Conky2
-Comment=<optional comment>
-EOF
+	cat <<EOF > "/root/.conkyrc_left"
+	# Conky Left
+	background no
+	use_xft yes
+	xftalpha 0.6
+	own_window true
+	own_window_type desktop
+	own_window_argb_visual true
+	# 0 = transparent, 255 = solid
+	own_window_argb_value 160 
+	double_buffer yes
+	update_interval 1
+	#maximum_width 400
+	alignment top_left
+	gap_x 60
+	gap_y 110
+	no_buffers yes
+	uppercase no
+	cpu_avg_samples 5
+	net_avg_samples 5
+	diskio_avg_samples 5
+	if_up_strictness address
+	draw_shades no
+	draw_outline no
+	draw_borders no
+	draw_graph_borders yes
+	default_color lightgray
+	default_shade_color red
+	default_outline_color green
+	short_units true
+	use_spacer none
+	xftfont DejaVu Sans Mono:size=10
+	TEXT
+	\${color white}Networking \$hr
+	# Start eth0
+	\${if_up eth0}\${color green}eth0 \$color \${addr eth0}
+	\${color green}eth0 \$color \${exec ifconfig eth0| grep ether | cut -d" " -f10 } \${endif}
+	#
+	# Start wlan0
+	\${if_up wlan0}
+	\${color green}wlan0 \$color \${exec iwgetid -r} 
+	\${color green}wlan0 \$color \${addr wlan0}
+	\${color green}wlan0 \$color \${exec ifconfig wlan0| grep ether | cut -d" " -f10 } 
+	#
+	\${color white}Upload Gateway: \$color\${upspeedf wlan0}Kb/s
+	\${color green}\${upspeedgraph wlan0 20,350 0000ff ff0000 -t}
+	\${color white}Download Gateway: \$color\${downspeedf wlan0}Kb/s
+	\${color green}\${downspeedgraph wlan0 20,350 0000ff ff0000 -t}
+	\${endif}
+	#
+	# Start ra0
+	\${if_up ra0}
+	\${color green}ra0 \$color \${exec iwgetid -r} 
+	\${color green}ra0 \$color \${addr ra0}
+	\${color green}ra0 \$color \${exec ifconfig ra0| grep ether | cut -d" " -f10 } 
+	\${color white}Upload Gateway: \$color\${upspeedf ra0}Kb/s
+	\${color green}\${upspeedgraph ra0 20,350 0000ff ff0000 -t}
+	\${color white}Download Gateway: \$color\${downspeedf ra0}Kb/s
+	\${color green}\${downspeedgraph ra0 20,350 0000ff ff0000 -t}
+	\${endif}
+	# Start tun0
+	\${if_up tun0}\${color green}tun0 \$color \${addr tun0} \${endif}
+	\${color white}Listening TCP:
+	\${color green}\${execi 10 netstat -anlp | grep LISTEN | grep -v ING | awk -F" " '{printf "%-5s %-15s %-15s %-15s\n", \$1, \$4, \$5, \$7}'}
+	\${color white}Listening UDP:
+	\${color green}\${execi 10 netstat -anulp | egrep -v "udp6|Proto|\(servers|ESTABLISHED" | awk -F" " '{printf "%-5s %-15s %-15s %-15s\n", \$1, \$4, \$5, \$6}'}
+	EOF
 
-# Start up items
-cat <<EOF > "/root/.config/autostart/conkyrc_left.desktop"
-[Desktop Entry]
-Type=Application
-Exec=/usr/bin/conky -q -c /root/.conkyrc_left
-Hidden=false
-NoDisplay=false
-X-GNOME-Autostart-enabled=true
-Name=Conky2
-Comment=<optional comment>
-EOF
+	mkdir -p /root/.config/autostart/
+	cat <<EOF > "/root/.config/autostart/conkyrc_right.desktop"
+	[Desktop Entry]
+	Type=Application
+	Exec=/usr/bin/conky -q -c /root/.conkyrc_right
+	Hidden=false
+	NoDisplay=false
+	X-GNOME-Autostart-enabled=true
+	Name=Conky2
+	Comment=<optional comment>
+	EOF
+
+	# Start up items
+	cat <<EOF > "/root/.config/autostart/conkyrc_left.desktop"
+	[Desktop Entry]
+	Type=Application
+	Exec=/usr/bin/conky -q -c /root/.conkyrc_left
+	Hidden=false
+	NoDisplay=false
+	X-GNOME-Autostart-enabled=true
+	Name=Conky2
+	Comment=<optional comment>
+	EOF
+}
 
 # reboot? # 
 echo;echo;echo;
