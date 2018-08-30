806bc6f645b218f1e44e01c7de21da9a97806b88
CBHue <CBHue@users.noreply.github.com>
2018-08-29 21:09:56 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -381,6 +381,7 @@ echo -e "\n $GREEN[+]$RESET Apptitude updates ..."
 apt-get -qq update 
 
 # Sources
+echo -e "\n $GREEN[+]$RESET Sublime key ..."
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
 
